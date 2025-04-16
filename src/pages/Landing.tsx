d3778f5b2b0cd05c66e1b68886dc5662e58baa0a
Shubham9049 <shubham.rajveer19@gmail.com>
2025-04-16 16:47:08 +0530

    add chnages in landing

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -9,6 +9,7 @@ import PropertySlider from "../components/offPlan";
 import Blog from "../components/blog";
 import ForSaleComponent from "../components/ForSale";
 import ForRentComponent from "../components/ForRent";
+import { Phone, MessageCircle } from "lucide-react";
 
 export const Landing = () => {
   return (
@@ -30,15 +31,17 @@ export const Landing = () => {
         </div>
         <div className="fixed bottom-0 left-0 w-full z-50 flex md:hidden">
           <a
-            href="tel:+918368573451"
-            className="w-1/2 bg-[#D10B0B] text-white text-center py-4 font-semibold hover:bg-red-700 transition-all"
+            href="tel:+97147702260"
+            className="w-1/2 bg-[#D10B0B] text-white text-center py-4 font-semibold hover:bg-red-700 transition-all flex items-center justify-center gap-2"
           >
+            <Phone size={20} />
             Call Me
           </a>
           <a
-            href="https://wa.me/+918368573451"
-            className="w-1/2 bg-[#1752B4] text-white text-center py-4 font-semibold hover:bg-blue-700 transition-all"
+            href="https://wa.me/+971509863828"
+            className="w-1/2 bg-[#1752B4] text-white text-center py-4 font-semibold hover:bg-blue-700 transition-all flex items-center justify-center gap-2"
           >
+            <MessageCircle size={20} />
             Contact Me
           </a>
         </div>
