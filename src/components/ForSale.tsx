54d7f514403334790cf64eb7f1a40e1beab91758
unknown <chandanpms@gmail.com>
2025-05-01 14:06:01 +0530

    forsale all pages

--- src/components/ForSale.tsx
+++ src/components/ForSale.tsx
@@ -5,15 +5,15 @@ import { FaArrowRight } from "react-icons/fa";
 
 export default function ForSaleComponent() {
   return (
-    <div className="max-w-6xl mx-auto p-2 mt-14">
-      <h2 className="text-2xl md:text-4xl text-white mb-6 text-center">
-        <i>Properties for Sale</i>
+    <div className="max-w-6xl mx-auto p-2 mt-14 font-raleway">
+      <h2 className="text-3xl md:text-5xl text-white mb-6 text-center font-thin border-b w-fit mx-auto">
+        Properties for Sale
       </h2>
       <div className=" grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 px-3">
         {propertiesData.slice(0, 8).map((property) => (
           <div
             key={property.title}
-            className="relative w-full h-[250px] sm:h-[300px] md:h-[250px] lg:h-[200px] overflow-hidden rounded-lg shadow-md"
+            className="relative w-full h-[250px] sm:h-[300px] font-light hover:shadow-md hover:shadow-[var(--primary-color)] md:h-[250px] lg:h-[200px] overflow-hidden rounded-lg shadow-md"
           >
             {/* Property Image */}
             <img
@@ -30,7 +30,7 @@ export default function ForSaleComponent() {
             {/* Arrow Button (Bottom Right) */}
             <a
               href={`/forsaleDetails/${property.title}`}
-              className="absolute bottom-2 right-2 border border-white rounded-lg text-white py-2 px-4 sm:px-5 sm:py-3 transition-transform inline-flex items-center justify-center"
+              className="absolute bottom-2 right-2 hover:border-[var(--primary-color)] hover:text-[var(--primary-color)] border border-white rounded-lg text-white py-2 px-4 sm:px-5 sm:py-3 transition-transform inline-flex items-center justify-center"
             >
               <FaArrowRight size={16} />
             </a>
@@ -38,10 +38,10 @@ export default function ForSaleComponent() {
         ))}
       </div>
       {/* View All Button */}
-      <div className="flex items-center justify-center">
+      <div className="flex items-center justify-center font-light">
         <a
           href="/forsale"
-          className="inline-block px-8 py-2 text-lg text-white bg-[--primary-color] rounded-3xl hover:opacity-80 mt-6"
+          className="inline-block px-8 py-2 text-lg text-white bg-[var(--primary-color)] rounded-3xl hover:opacity-80 mt-6"
         >
           View All
         </a>
--- src/pages/ForSaleDetails.tsx
+++ src/pages/ForSaleDetails.tsx
@@ -11,11 +11,15 @@ function ForSaleDetails() {
   const property = salesData.find((item) => item.title === decodedTitle);
 
   if (!property) {
-    return <div>Property not found</div>;
+    return (
+      <div className="text-white bg-black h-screen flex justify-center items-center">
+        Property not found
+      </div>
+    );
   }
 
   return (
-    <div>
+    <div className="bg-black text-white min-h-screen font-raleway font-thin">
       <div className="mb-24">
         <Navbar />
       </div>
@@ -27,12 +31,10 @@ function ForSaleDetails() {
           alt=""
           className="h-[400px] w-full object-cover"
         />
-        <div className="  absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center p-8">
-          <div className=" text-white">
-            <h1 className="text-3xl font-bold md:pl-7">{property.title}</h1>
-            <p className="text-lg mt-2 md:pl-7 text-center">
-              {property.location}
-            </p>
+        <div className="absolute inset-0 bg-black bg-opacity-60 flex items-center justify-center p-8">
+          <div className="text-white text-center">
+            <h1 className="text-4xl">{property.title}</h1>
+            <p className="text-xl mt-2">{property.location}</p>
           </div>
         </div>
       </div>
@@ -44,58 +46,51 @@ function ForSaleDetails() {
             key={idx}
             src={img}
             alt={`property-${idx}`}
-            className=" rounded-xl h-60 object-cover w-full"
+            className="rounded-xl h-60 object-cover w-full"
           />
         ))}
       </div>
+
       {/* Property Overview */}
-      <div className="w-[90%] mx-auto my-10 grid grid-cols-1 sm:grid-cols-2 gap-4 text-gray-800 text-base">
+      <div className="w-[90%] mx-auto my-10 grid grid-cols-1 sm:grid-cols-2 gap-4 text-white">
         {/* Property Type */}
         <div className="flex items-center gap-3">
-          <FaThLarge className="text-xl text-gray-700" />
-          <span className="text-gray-600">Property Type:</span>
-          <span className="font-semibold text-black ml-2">
-            {property.propertyType}
-          </span>
+          <FaThLarge className="text-xl text-gray-300" />
+          <span className="text-gray-100">Property Type:</span>
+          <span className=" text-white ml-2">{property.propertyType}</span>
         </div>
 
         {/* Property Size */}
         <div className="flex items-center gap-3">
-          <FaRulerCombined className="text-xl text-gray-700" />
-          <span className="text-gray-600">Property Size:</span>
-          <span className="font-semibold text-black ml-2">
-            {property.areaSqft} Sq Ft
-          </span>
+          <FaRulerCombined className="text-xl text-gray-300" />
+          <span className="text-gray-100">Property Size:</span>
+          <span className=" text-white ml-2">{property.areaSqft} Sq Ft</span>
         </div>
 
         {/* Bedrooms */}
         <div className="flex items-center gap-3">
-          <FaBed className="text-xl text-gray-700" />
-          <span className="text-gray-600">Bedrooms:</span>
-          <span className="font-semibold text-black ml-2">
-            {property.bedrooms} Bed
-          </span>
+          <FaBed className="text-xl text-gray-300" />
+          <span className="text-gray-100">Bedrooms:</span>
+          <span className=" text-white ml-2">{property.bedrooms} Bed</span>
         </div>
 
         {/* Bathrooms */}
         <div className="flex items-center gap-3">
-          <FaBath className="text-xl text-gray-700" />
-          <span className="text-gray-600">Bathrooms:</span>
-          <span className="font-semibold text-black ml-2">
-            {property.bathrooms} Baths
-          </span>
+          <FaBath className="text-xl text-gray-300" />
+          <span className="text-gray-100">Bathrooms:</span>
+          <span className=" text-white ml-2">{property.bathrooms} Baths</span>
         </div>
       </div>
 
-      {/* Description + Sales Manager Side-by-Side */}
+      {/* Description + Sales Manager */}
       <div className="w-[90%] mx-auto my-10 flex flex-col lg:flex-row gap-8">
         {/* Left: Description */}
         <div className="lg:w-2/3">
-          <h2 className="text-xl font-bold mb-2">Description</h2>
-          <p className="text-gray-700 mb-6">{property.description.short}</p>
+          <h2 className="text-2xl  mb-2">Description</h2>
+          <p className="text-gray-200 mb-6">{property.description.short}</p>
 
-          <h3 className="text-lg font-semibold mb-2">Features</h3>
-          <ul className="list-disc pl-6 text-gray-700 space-y-2">
+          <h3 className="text-2xl mb-2">Features</h3>
+          <ul className="list-disc pl-6 text-gray-200 space-y-2">
             {property.description.features.map((feature, idx) => (
               <li key={idx}>{feature}</li>
             ))}
@@ -103,15 +98,15 @@ function ForSaleDetails() {
         </div>
 
         {/* Right: Sales Manager */}
-        <div className="lg:w-1/3 bg-gray-100 p-6 rounded-xl shadow-md">
+        <div className="lg:w-1/3 bg-gray-700 p-6 rounded-xl shadow-md text-white">
           <div className="flex flex-col items-center text-center">
             <img
-              src="https://i.pravatar.cc/150?img=32" // Use a real image or replace with actual
+              src="https://i.pravatar.cc/150?img=32"
               alt="Sales Manager"
               className="w-24 h-24 rounded-full mb-4"
             />
-            <h4 className="text-lg font-semibold">{property.salesManager}</h4>
-            <p className="text-sm text-gray-600 mb-4">{property.agency}</p>
+            <h4 className="text-xl">{property.salesManager}</h4>
+            <p className="text-sm text-gray-200 mb-4">{property.agency}</p>
             <a
               href={property.contact.link}
               target="_blank"
@@ -124,23 +119,22 @@ function ForSaleDetails() {
         </div>
       </div>
 
-      <div className="bg-gray-100 py-10 mt-10">
+      {/* Contact Banner */}
+      <div className="bg-black py-10 mt-10 ">
         <div className="max-w-6xl mx-auto px-4 flex flex-col md:flex-row items-center justify-between gap-6">
           {/* Text */}
-          <div className="text-center md:text-left">
-            <h2 className="text-2xl font-semibold text-gray-800 mb-2">
-              Have a question?
-            </h2>
-            <p className="text-gray-600">Our team is happy to assist you</p>
+          <div className="text-center md:text-left font-raleway font-thin">
+            <h2 className="text-3xl text-white mb-2">Have a question?</h2>
+            <p className="text-gray-200">Our team is happy to assist you</p>
           </div>
 
           {/* Contact Actions */}
           <div className="flex flex-col sm:flex-row items-center gap-4">
-            <button className="bg-[var(--primary-color)] hover:opacity-70 text-white px-6 py-2 rounded-md transition">
+            <button className="bg-[var(--primary-color)] hover:opacity-70 text-white px-6 py-2 rounded-md transition font-raleway font-thin">
               <a href="/contact">Contact Us</a>
             </button>
-            <span className="text-lg font-medium text-gray-800">
-              📞 +1 (800) 123-4567
+            <span className="text-lg font-medium text-gray-300">
+              📞 +97147702260
             </span>
           </div>
         </div>
